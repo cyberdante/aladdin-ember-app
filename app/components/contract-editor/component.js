fc12bd787019d29bc742563ad38d16abb5bfe8fc
Damian Perez <cyberdante@gmail.com>
2018-07-23 11:27:28 -0400

    Debounce contract editor input changes

--- app/components/contract-editor/component.js
+++ app/components/contract-editor/component.js
@@ -1,12 +1,19 @@
 import Component from '@ember/component';
+import { debounce } from '@ember/runloop';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
     classNames: ['md-padding'],
     value: '',
     newValue: '',
+    setUpdatedValueLazily(newValue) {
+        console.log(newValue);
+    },
+
     actions: {
         valueUpdated(newValue) {
-            console.log(newValue);
+            let component = this;
+            debounce(component, component.setUpdatedValueLazily, newValue, 500);
         }
     }
 });
