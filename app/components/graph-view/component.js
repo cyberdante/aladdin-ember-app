ebf8fbef1ee32a226350e3f3d7e96026ecfe28fb
Adam Brinckman <abrinckm@gmail.com>
2018-07-24 13:25:38 -0400

    playing with colors in graph view

--- app/components/graph-view/component.js
+++ app/components/graph-view/component.js
@@ -5,13 +5,11 @@ import Viz from 'viz';
 import { Module, render } from 'viz-full-render';
 
 export default Component.extend({
-  tagName: 'svg',
-  attributeBindings: ['xmlns', 'xmlns:xlink', 'width', 'height', 'viewBox'],
-
   blockchainUtils: service(),
 
-  viz: null,
   schema: '',
+  viz: null,
+  svg: null,
 
   init() {
     this._super(...arguments);
@@ -25,12 +23,7 @@ export default Component.extend({
       const self = this;
       let graph = this.blockchainUtils.generateGraph(this.schema);
       this.viz.renderSVGElement(graph).then(svg => {
-        self.set('viewBox', svg.getAttribute('viewBox'));
-        self.set('width', svg.getAttribute('width'));
-        self.set('height', svg.getAttribute('height'));
-        self.set('xmlns:xlink', svg.getAttribute('xmlns:xlink'));
-        self.set('xmlns', svg.getAttribute('xmlns'));
-        self.element.appendChild(svg.children[0]);
+        self.set('svg', svg);
       });
     }
   }
