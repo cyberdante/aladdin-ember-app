a2efde8495ced14372297a16450e4b187e813461
Sebastian Wyngaard <swyngaar@nd.edu>
2018-07-27 00:27:04 -0400

    Specify dev and prod assets for Solidity syntax highlighter

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -59,7 +59,11 @@ module.exports = function(defaults) {
     ]
   });
 
-  app.import('node_modules/ace-mode-solidity/build/v1.3.3/src-noconflict/mode-solidity.js', { outputFile: 'mode-solidity.js' });
+  app.import({
+    development: 'node_modules/ace-mode-solidity/build/v1.3.3/src-noconflict/mode-solidity.js',
+    production: 'node_modules/ace-mode-solidity/build/v1.3.3/src-min-noconflict/mode-solidity.js',
+    outputFile: 'mode-solidity.js'
+  });
 
   return app.toTree();
 };
