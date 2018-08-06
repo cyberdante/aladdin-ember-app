dae15ec72166dd43f4122c486e83c71cb2e9da3a
Adam Brinckman <abrinckm@gmail.com>
2018-08-06 11:34:48 -0400

    show headers when appropriate in asset view

--- app/components/assets-transactions-wizard/component.js
+++ app/components/assets-transactions-wizard/component.js
@@ -1,10 +1,8 @@
 import Component from '@ember/component';
-// import { observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { A } from '@ember/array';
 import O from '@ember/object';
 import { observer } from '@ember/object';
-import { scheduleOnce } from '@ember/runloop';
 
 export default Component.extend({
   blockchainUtils: service(),
@@ -22,7 +20,6 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    // this.send('onViewChange', '{"$schema":"http://json-schema.org/draft-04/schema","title":"Voting","description":"Smart Contract Form for the demo","type":"object","properties":{"vote":{"type":"object","properties":{"uid":{"name":"uid","type":"string"},"candidateID":{"name":"candidateID","type":"number"},"dependencies":{"assetId":{"name":"assetId","type":"candidate"}},"returns":{}},"title":"castVote"},"totalVotes":{"type":"object","properties":{"candidateID":{"name":"candidateID","type":"number"},"dependencies":{"assetId":{"name":"assetId","type":"candidate"}},"returns":{"name":"","type":"number"}},"title":"totalVotes"},"addCandidate":{"type":"object","properties":{"name":{"name":"name","type":"string"},"party":{"name":"party","type":"string"},"dependencies":{"assetId":{"name":"assetId","type":"candidate"}},"returns":{}},"title":"addCandidate"},"AddedCandidate":{"type":"object","properties":{"candidateID":{"indexed":false,"name":"candidateID","type":"number"}},"title":"AddedCandidate"}}}');
   },
 
   didInsertElement() {
