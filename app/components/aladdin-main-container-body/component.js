9784dbd5f3358213d57cdbc61c5a8c865bb0b618
srucker01 <srucker94@gmail.com>
2018-09-06 13:15:40 -0400

    container yaml

--- app/components/aladdin-main-container-body/component.js
+++ app/components/aladdin-main-container-body/component.js
@@ -11,10 +11,17 @@ export default Component.extend({
     blockchainUtils: service(),
     schema: '',
     yaml: `---
-    - asset:  &container                 # defines anchor label
+    - asset:  &Container                 # defines anchor label
           name:   assetId
-          type:   container
-     
+          type:   Container
+    - asset:  &Lock                
+          name:   assetId
+          type:   Lock
+    - asset:  &Manifest                
+          name:   assetId
+          type:   Manifest
+    
+    
     - transaction: 
        properties: object
        arrived:
@@ -22,12 +29,145 @@ export default Component.extend({
         properties:				# method variable
           sNum:
             name: sNum
-            type: uint
+            type: number
           arrived:
             name: arrived
-            type: uint
-          dependencies:  *container
+            type: number
+          dependencies:  *Container
         title: arrived
+    
+    - transaction: 
+       properties: object
+       tampered:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          tampered:
+            name: tampered
+            type: number
+          dependencies:  *Lock
+        title: tampered
+    
+    - transaction: 
+       properties: object
+       lock:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          locked:
+            name: locked
+            type: number
+          dependencies:  *Container
+        title: lock
+    
+    - transaction: 
+       properties: object
+       idle:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          idle:
+            name: idle
+            type: number
+          dependencies:  *Container
+        title: idle
+    
+    - transaction: 
+       properties: object
+       unlock:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          tampered:
+            name: tampered
+            type: number
+          unlocked:
+            name: unlocked
+            type: number
+          dependencies:  *Lock
+        title: unlock
+    
+    - transaction: 
+       properties: object
+       locked:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          tampered:
+            name: tampered
+            type: number
+          locked:
+            name: locked
+            type: number
+          dependencies:  *Lock
+        title: locked
+     
+    - transaction: 
+       properties: object
+       saveManifest:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          carNum:
+            name: carNum
+            type: number
+          dependencies:  *Manifest
+        title: saveManifest
+    
+    - transaction: 
+       properties: object
+       departed:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          departed:
+            name: departed
+            type: number
+          dependencies:  *Container
+        title: departed
+    
+    - assignAsset: 
+       properties: object
+       manifest:
+        type: object
+        properties:
+          contents:
+            name: contents
+            type: string
+          locked:
+            name: locked
+            type: number
+          dependencies:  *Container
+        title: manifest
+    
+    - transaction: 
+       properties: object
+       received:
+        type: object
+        properties:
+          sNum:
+            name: sNum
+            type: number
+          received:
+            name: received
+            type: number
+          dependencies:  *Container
+        title: received
+        
         
   `,
 
