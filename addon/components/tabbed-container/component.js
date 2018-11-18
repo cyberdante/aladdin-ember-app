9bcc08a35a790c855a0786558b957a411bb83a2a
Damian Perez <cyberdante@gmail.com>
2018-11-18 12:52:22 -0500

    Clean unnecessary commented code

--- addon/components/tabbed-container/component.js
+++ addon/components/tabbed-container/component.js
@@ -1,13 +1,10 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import layout from './template';
 
 export default Component.extend({
     layout,
     classNames: ['md-padding', 'tabbed-container'],
-    // selectedTab: 0,
-    // selectedComponent: 'contract-viewer',
-    // tabTitles: computed(() => ['Smart Contract', 'Graph']),
+    
     init() {
         this._super(...arguments);
     },
